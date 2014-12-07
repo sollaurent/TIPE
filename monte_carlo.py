5b72a6b1f2954377cd012f46be454ec04a98f067
sollaurent <sollaurent@users.noreply.github.com>
2014-12-07 17:02:19 +0100

    Update monte_carlo.py
    Les paramètres d'entrée pourront être modifies quand j'aurais fait l'algo des équations thermo et que je saurais exactement ce dont j'ai besoin. Les calculs des pressions et températures corespondant à l'altitude peuvent être fait dans cet algorithme s besoin.
    Pour ne pas partir complètement dans les choux lors des premiers essais, il faut définir globalement les valeurs admissibles, possibles, réalisable...

--- monte_carlo.py
+++ monte_carlo.py
@@ -1,4 +1,19 @@
-def monte_carlo(calcul_rendement):
-    L=[1,2]
-    print L
-    return 0
+def monte_carlo(N):
+"Prend en entree le nombre d'essais N
+"Retourne une liste contenant N listes de paramètres d'entrees
+"Chaque liste correspond a une experience, les parametres etant choisis au hasard
+    entrees=[]
+    for i in range (N):
+        experience=[]
+        z=random.randint(-2000,50000)       #altitude
+        M=random(0,2)                       #nb de Mach
+        VC=random()                         #vitesse conventionnelle_domaine de test a preciser
+        VA=random()                         #vitesse aerodynamique_domaine de test a preciser
+        ISA=random()                        #ecart de temperature/atmosphere standard_domaine de test a preciser
+        experience.append(z,M,VC,VA,ISA)
+        entrees.append experience
+    return entrees
+    
+    
+    
+
