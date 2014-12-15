66073ed2258ab25b46ffde398cef69af2412900f
GTaf <auredu94@gmail.com>
2014-12-15 12:07:19 +0100

    Update and rename regression multivariable to regression_multivariable.py

--- regression_multivariable.py
+++ regression_multivariable.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 from statsmodels.formula.api import ols*
 
-def regression multivariable(s,x,y,z):
+def regression_multivariable(s,x,y,z):
   df = pd.DataFrame({'x':X, 'y':Y, 'z':Z})
   model = ols("z ~ x + y", df).fit() #trouve le modèle
 
