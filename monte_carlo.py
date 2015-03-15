32d61f4baeb15867960a6215925532ef50c2462e
GTaf <auredu94@gmail.com>
2015-03-15 23:13:57 +0100

    Update monte_carlo.py
    Ca fonctionne

--- monte_carlo.py
+++ monte_carlo.py
@@ -1,4 +1,5 @@
-
+from Modèle import *
+from random import *
 
 #fonction donnant la température et la pression en fonction de l'altitude    
 ISA_temp = interpolate.interp1d([0,11000,20000,32000],[288,216.5,216.5,228.5])
@@ -6,10 +7,12 @@ ISA_P = interpolate.interp1d([0,11000,20000,32000],[101325,22632,5474.9,868.02])
 
 
 def monte_carlo(N):
-"Prend en entree le nombre d'essais N
-"Retourne une liste contenant N listes de paramètres d'entrees
-"Chaque liste correspond a une experience, les parametres etant choisis au hasard
+    """Prend en entree le nombre d'essais N
+    Retourne une liste contenant N listes de paramètres d'entrees
+    Chaque liste correspond a une experience, les parametres etant choisis au 
+    hasard"""
     entrees=[]
+    resultat = open("resulat.txt","a")#cré fichier resultat
     for i in range (N):
         experience=[]
         
@@ -20,7 +23,7 @@ def monte_carlo(N):
         PC= ISA_P(z)
         
         #Nombre de Mach
-        M=random(0.5,2.5)
+        M=uniform(0.5,2.5)#loi uniforme sur 0.5 2.5
         #Temperature totale
         TT=TC*(1+0.2*M**2)
         #Pression totale
@@ -31,23 +34,18 @@ def monte_carlo(N):
         tchp=randint(1,10)
         tt=randint(1,10)
         #Richesse
-        alpha=random(0,1)
+        alpha=random()
         #Coefficient de partage du flux
-        lamb=random(0,1)
+        lamb=random()
         #Flux
-        WA=randint(100;1000)
-        WF=randint(100,1000)
+        WA=randint(100,1000)
+        WF=uniform(0.01,0.1)
         #Vitesse
         VA=M*(1.4*237*TC)**0.5
         a=VA/M        
         
-        experience.append(TT,PT,ts,tcbp,thbp,tt,alpha,lamb,WA,WF,a,turboreacteur(TT,PT,ts,tcbp,thbp,tt,alpha,lamb,WA,WF,a))
+        experience=[TT,PT,ts,tcbp,tchp,tt,alpha,lamb,WA,WF,a,turboreacteur(TT,PT,ts,tcbp,tchp,tt,alpha,lamb,WA,WF,a)]
         entrees.append(experience)
+        resultat.write(str(experience))#ajoute au fichier
+    resultat.close()
     return entrees
-    
-    
-T1,P1,ts,tcbp,tchp,tt,alpha,lamb,WA,WF,a
-    
-    
-    
-
