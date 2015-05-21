16b816a2c3c251de78c19a058d69b4d62251ca05
sollaurent <sollaurent@users.noreply.github.com>
2015-05-21 09:19:21 +0200

    Update Modèle.py

--- Modèle.py
+++ Modèle.py
@@ -10,13 +10,13 @@ def gamma_temperature(fluide,alpha,a):
     la vitesse du son a.
     Renvoi gamma(T)"""
     if fluide==0:
-        Cpr=lambda T: 3.5-0.000028*T+0.0000000224*T*T+(3000*3000*a)/(T*(a-1)*(a-1))
+        Cpr=lambda T: 3.5-0.000028*T+0.0000000224*T*T+(3090*3090*a)/(T*T*(a-1)*(a-1))
         gamma=lambda T: Cpr(T)/(Cpr(T)-1)
     if fluide==1:
         Cpr=lambda T: -0.0000018373*T*T+0.00801994*T+4.47659
         gamma=lambda T: Cpr(T)/(Cpr(T)-1)
     if fluide==2:
-        Cpra=lambda T: 3.5-0.000028*T+0.0000000224*T*T+(3000*3000*a)/(T*(a-1)*(a-1))
+        Cpra=lambda T: 3.5-0.000028*T+0.0000000224*T*T+(3090*3090*a)/(T*T*(a-1)*(a-1))
         Cprk=lambda T: -0.0000018373*T*T+0.00801994*T+4.47659
         Cpr=lambda T: (Cpra(T)+alpha*Cprk(T))/(alpha+1)
         gamma=lambda T: Cpr(T)/(Cpr(T)-1)
@@ -28,18 +28,19 @@ def Cp_temperature(fluide,alpha,a):
     la vitesse du son a.
     Renvoi Cp(T)"""
     if fluide==0:
-        Cpr=lambda T: 3.5-0.000028*T+0.0000000224*T*T+(3000*3000*a)/(T*(a-1)*(a-1))
+        Cpr=lambda T: 3.5-0.000028*T+0.0000000224*T*T+(3090*3090*a)/(T*T*(a-1)*(a-1))
     if fluide==1:
         Cpr=lambda T: -0.0000018373*T*T+0.00801994*T+4.47659
     if fluide==2:
-        Cpra=lambda T: 3.5-0.000028*T+0.0000000224*T*T+(3000*3000*a)/(T*(a-1)*(a-1))
+        Cpra=lambda T: 3.5-0.000028*T+0.0000000224*T*T+(3090*3090*a)/(T*T*(a-1)*(a-1))
         Cprk=lambda T: -0.0000018373*T*T+0.00801994*T+4.47659
         Cpr=lambda T: (Cpra(T)+alpha*Cprk(T))/(alpha+1)
     return Cpr
     
-def turboreacteur(T1,P1,ts,tcbp,tchp,tt,alpha,lamb,WA,WF,a):
+def turboreacteur(T1,P1,ts,tcbp,tchp,Tcomb,lamb,WA,VA,rs,rcbp,rchp,rtbp,rthp):
     """Modélisation d'un turboréacteur
-    Hypothèses : transformations isentropiques dans com  """
+    Hypothèses : transformations isentropiques dans compresseurs
+    avec prise en compte des rendements   """
     
     #calcul des fonctions utilisés dans les blocs du turboréacteur
     fp1=lambda T: gamma_temperature(0,0,a)(T)/(T*(gamma_temperature(0,0,a)(T)-1)) #fonction gamma/T*(gamma-1) de l'air
@@ -68,33 +69,38 @@ def turboreacteur(T1,P1,ts,tcbp,tchp,tt,alpha,lamb,WA,WF,a):
     DfCO2=394000#DfCO2
     DfH2O=280000#DfH20
     DfCH4=88000
-    avanct=WF/(0.012+4*0.001)
-    Df=avanct*(DfCO2+2*DfH2O-DfCH4)
+    DH=DfCO2-DfCH4-2*DfH2O #enthalpie de reaction
+    MCH4(0.012+4*0.001)#masse molaire CH4
+    T5=Tcomb
     
+    alpha=MCH4/(WA*DH)*(f2(T5)-f2(T4))
+    WF=alpha*WA
+
     print((T4))
     
-    T5=finv2(f2(T4)+Df)
+    #T5=finv2(f2(T4)+Df)
     
     print(T5)
     
     #Turbine HP, obtenu par equilibre HP
-    T6=finv2(f2(T5)+(f2(T4)-f2(T3))*WA/(WA+WF))
+    T6=finv2(f2(T5)-(f2(T4)-f2(T3))*WA/(WA+WF)/rchp/rthp)
     
     #Turbine BP, obtenu par equilibre BP
-    T7=finv2(f2(T6)+(f2(T3)-f2(T1))*WA/(WA+WF)+(f2(T1)-f2(T2))*lamb*WA/(WA+WF))
+    T7=finv2(f2(T6)-(f2(T3)-f2(T1))*WA/(WA+WF)/rcbp/rtbp-(f2(T2)-f2(T1))*lamb*WA/(WA+WF)/rs/rtbp)
     
     #Mélangeur, application 1er principe
     T8=finv2(((WA+WF)*f2(T7)+lamb*WA*f2(T2))/(WF+WA+WA*lamb))
+
     
     #Tuyère, application 1er principe
     T9=finv1(f1(T8)+log(tt))
     C9=sqrt(2+f2(T9)-f2(T8))
     
     #Rendement
-    Pcin=(WA+lamb*WA+WF)*C9*C9/2
+    Pcin=((WA+lamb*WA+WF)*C9*C9-(1+lamb)*WA*VA*VA)/2
     Pth=(WA+WF)*(f2(T5)-f2(T4))
     Rendement=Pcin/Pth
     
-    return Rendement
+    return Rendement,alpha
     
     
